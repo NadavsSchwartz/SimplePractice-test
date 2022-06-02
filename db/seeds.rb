f92e964850359195cffdd5d7e73a1a6ff17db1ca
nadavschwartz <nadavschwartz58@gmail.com>
2022-06-02 10:27:15 -0700

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# TODO: Seed the database according to the following requirements:
+
+# - There should be 10 Doctors with unique names
+
+# - Each patient should have 10 appointments
+# - 5 in the past, 5 in the future
+# - Each appointment should be 50 minutes in duration
