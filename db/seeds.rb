af9dac7d558c74ddf3b44c2275a56f91ac8dc8d7
nadavschwartz <nadavschwartz58@gmail.com>
2022-06-02 10:47:41 -0700

    added basic seed data per requirements

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,46 @@
 
 # - There should be 10 Doctors with unique names
 
+10.times { Doctor.create(name: Faker::Name.unique.name) }
+
+# - Each doctor should have 10 patients with unique names
+Doctor.all.each do |doctor|
+	10.times do
+		doctor.patients.create(name: Faker::Name.unique.name, doctor_id: doctor.id)
+	end
+end
+
 # - Each patient should have 10 appointments
 # - 5 in the past, 5 in the future
 # - Each appointment should be 50 minutes in duration
+
+#add id to patients
+
+Patient.all.each do |patient|
+	5.times do
+		patient.appointments.create(
+			start_time:
+				Faker::Time.between(
+					from: DateTime.now + 15.days,
+					to: DateTime.now + 25.days,
+					format: :default,
+				),
+			duration_in_minutes: 50,
+			patient_id: patient.id,
+			doctor_id: patient.doctor_id,
+		)
+	end
+	5.times do
+		patient.appointments.create(
+			start_time:
+				Faker::Time.between(
+					from: DateTime.now - 25.days,
+					to: DateTime.now - 15.days,
+					format: :default,
+				),
+			duration_in_minutes: 50,
+			patient_id: patient.id,
+			doctor_id: patient.doctor_id,
+		)
+	end
+end
