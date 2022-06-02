0c4fb0112de8251f46b9a13c5e48ba96131d35a1
nadavschwartz <nadavschwartz58@gmail.com>
2022-06-02 10:29:34 -0700

    add unique name validation for doctor / patient model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,5 @@
 class Doctor < ApplicationRecord
+  validates :name, uniqueness: true
   has_many :patients
   has_many :appointments
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,6 @@
 # For this test app, you can assume a patient has only one doctor
 class Patient < ApplicationRecord
+  validates :name, uniqueness: true
   belongs_to :doctor
   has_many :appointments
 end
