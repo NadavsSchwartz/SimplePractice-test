d5fcc4c6ff50a908daef072f56740ab085b283bc
nadavschwartz <nadavschwartz58@gmail.com>
2022-06-02 10:52:31 -0700

    initial logic for returning all or filtered appointments depending on params

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,11 +1,48 @@
 class Api::AppointmentsController < ApplicationController
-  def index
-    # TODO: return all values
-    # TODO: return filtered values
-    head :ok
-  end
+	has_scope :past, type: :boolean, using: %i[past], type: :hash
 
-  def create
-    # TODO:
-  end
+	has_scope :page, type: :integer, using: %i[page], type: :hash
+	def index
+		# if params exist, use it to filter appointments
+		if (params[:past])
+			@appointments = Appointment.past(params[:past])
+		elsif (params[:page] && params[:length])
+			@appointments = Appointment.page(params[:page], params[:length])
+		else
+			# if params doesn't exists, return all appointments
+			@appointments = Appointment.all
+		end
+		return render json: join_tables_and_modify_response(@appointments)
+	end
+
+	def create
+		# TODO:
+	end
+
+	private
+
+	def join_tables_and_modify_response(appointments)
+		return(
+			appointments
+				.joins(:patient, :doctor)
+				.select(
+					'appointments.*, patients.name as patient_name, patients.id as patient_id, doctors.name as doctor_name, doctors.id as doctor_id',
+				)
+				.map do |appointment|
+					{
+						id: appointment.id,
+						patient: {
+							name: appointment.patient_name,
+						},
+						doctor: {
+							name: appointment.doctor_name,
+							id: appointment.doctor_id,
+						},
+						created_at: appointment.created_at.iso8601,
+						start_time: appointment.start_time.iso8601,
+						duration_in_minutes: appointment.duration_in_minutes,
+					}
+				end
+		)
+	end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,17 @@
 class Appointment < ApplicationRecord
-  belongs_to :doctor
-  belongs_to :patient
+	belongs_to :doctor
+	belongs_to :patient
+
+	scope :past,
+	      ->(past) {
+			if past == '1'
+				@past_appointments = where('start_time > ?', Time.now)
+			else
+				@past_appointments = where('start_time < ?', Time.now)
+			end
+			@past_appointments
+	      }
+
+	scope :page,
+	      ->(page, length) { limit(length).offset(page.to_i * length.to_i) }
 end
