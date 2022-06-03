c0367bc3d58f5fcba972b9c570ffb559bb1327a6
nadavschwartz <nadavschwartz58@gmail.com>
2022-06-03 12:15:39 -0700

    updated seed file functionallity to be slightly efficient, reducing DB calls

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,33 @@
 # TODO: Seed the database according to the following requirements:
 
-# - There should be 10 Doctors with unique names
 
-10.times { Doctor.create(name: Faker::Name.unique.name) }
+ 
 
-# - Each doctor should have 10 patients with unique names
-Doctor.all.each do |doctor|
-	10.times do
-		doctor.patients.create(name: Faker::Name.unique.name, doctor_id: doctor.id)
-	end
-end
-
-# - Each patient should have 10 appointments
-# - 5 in the past, 5 in the future
-# - Each appointment should be 50 minutes in duration
-
-#add id to patients
-
-Patient.all.each do |patient|
-	5.times do
-		patient.appointments.create(
-			start_time:
-				Faker::Time.between(
-					from: DateTime.now + 15.days,
-					to: DateTime.now + 25.days,
-					format: :default,
-				),
-			duration_in_minutes: 50,
-			patient_id: patient.id,
-			doctor_id: patient.doctor_id,
-		)
-	end
-	5.times do
-		patient.appointments.create(
-			start_time:
-				Faker::Time.between(
-					from: DateTime.now - 25.days,
-					to: DateTime.now - 15.days,
-					format: :default,
-				),
-			duration_in_minutes: 50,
-			patient_id: patient.id,
-			doctor_id: patient.doctor_id,
-		)
-	end
-end
+10.times do
+  # # - There should be 10 Doctors with unique names
+  doctor = Doctor.create(name: Faker::Name.unique.name)
+  10.times do
+    # - Each doctor should have 10 patients with unique names
+    patient = doctor.patients.create(name: Faker::Name.unique.name)
+    5.times do
+      # - Each patient should have 5 appointments  5 in the future
+      patient.appointments.create(
+        start_time: Faker::Time.between(from: 5.days.from_now, to: 20.days.from_now),
+        # - Each appointment should be 50 minutes in duration
+        duration_in_minutes: 50,
+        doctor_id: doctor.id,
+        patient_id: patient.id,
+      )
+    end
+    # - Each patient should have 5 appointments  5 in the past
+      5.times do
+        patient.appointments.create(
+          start_time: Faker::Time.between(from: 20.days.ago, to: 5.days.ago),
+          # - Each appointment should be 50 minutes in duration
+          duration_in_minutes: 50,
+		  doctor_id: doctor.id,
+		  patient_id: patient,
+        )
+      end
+    end
+  end
