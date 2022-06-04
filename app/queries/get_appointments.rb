8ff1c85c894a1b6bbcff0359e87a2ba699d8ef72
nadavschwartz <nadavschwartz58@gmail.com>
2022-06-04 16:14:30 -0700

    updated get_appointments queries

--- app/queries/get_appointments.rb
+++ app/queries/get_appointments.rb
@@ -1,37 +1,43 @@
-
 class GetAppointments
-    attr_accessor :initial_scope
+  attr_accessor :initial_scope
 
-    def initialize(initial_scope)
- 
-      @initial_scope = initial_scope
-    end
+  def initialize(initial_scope)
+    @initial_scope = initial_scope
+  end
 
   def call(params)
-    scoped = get_all_past_appointments(@initial_scope, params[:past]) 
-    scoped = get_all_appointments_paginated(scoped, params[:page], params[:length])
-    scoped
-  end
- 
-  private def get_all_past_appointments(scoped, past)
-    if past && !['0', '1'].include?(past)
-        return { error: 'past must be 0 or 1' }
-    end
-    scoped.where("start_time #{past == '0' ? '<' : '>'} ?", Time.now) 
+    scoped = get_all_past_appointments(@initial_scope, params[:past])
+    get_all_appointments_paginated(scoped, params[:page], params[:length])
   end
 
-    private def get_all_appointments_paginated(scoped, page = nil, length = nil)
-     
-        if page.nil? ^ length.nil?
-            return { error: 'page and length must be present together' }
-            end
-        
-        if page && length
-            if page.to_i.to_s != page || length.to_i.to_s != length
-                return { error: 'page and length must be integers' }
-            end
-        end
-        
-        scoped.limit(length).offset((page.to_i) * length.to_i) 
+  private def get_all_past_appointments(scoped, past)
+    # if past param exists but not valid, return error
+    if past && !%w[0 1].include?(past)
+      { error: 'past must be 0 or 1' }
+    # if past param exists and is valid, return all appointments based on past param
+    elsif past && %w[0 1].include?(past)
+      scoped.where("start_time #{past == '0' ? '>' : '<'} ?", Time.now)
+    else
+      # if past param does not exist, return all appointments
+      scoped
     end
+  end
+
+  private def get_all_appointments_paginated(scoped, page = nil, length = nil)
+    # if only one of page or length params exists, return error
+    { error: 'page and length must be present together' } if page.nil? ^ length.nil?
+    # if both page and length params exist but not valid, return error
+    if page && length
+      if page.to_i < 0 || length.to_i < 0
+        { error: 'page and length must be positive integers' }
+
+      # if both page and length params exist and are valid, return paginated appointments
+      else
+        scoped.limit(length).offset(page.to_i * length.to_i)
+      end
+    else
+      # if neither page or length params exist, return all appointments
+      scoped
+    end
+  end
 end
