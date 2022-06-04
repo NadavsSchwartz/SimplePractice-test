9247d0caf7dc9f9260d4ff4938cd4d8ce4dc9aeb
nadavschwartz <nadavschwartz58@gmail.com>
2022-06-04 16:09:06 -0700

    updated validation for start_time, duration_in_minutues to properly validate when creating new appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
 
 class Appointment < ApplicationRecord
-  
+  validate :start_time_must_be_in_the_future, on: :create
   belongs_to :doctor
   belongs_to :patient
 
+  # validates start time is valid date format
+  validates :start_time, presence: true, format: { with: %r{(\d{1,2}[-/]\d{1,2}[-/]\d{4})|(\d{4}[-/]\d{1,2}[-/]\d{1,2})}, message: 'must be a valid date format' }
+
+  # validates duration is a number and equal to 50
+  validates :duration_in_minutes, presence: true, numericality: { only_integer: true, equal_to: 50, message: 'must be 50 minutes' }, on: :create
+
+  # validates start time is in the future
+  def start_time_must_be_in_the_future
+    errors.add(:start_time, 'must be in the future') if start_time.present? && start_time < Time.now - 1.minute
+  end
+
+  # joins patient and doctor tables and modifies response per requirements
   def self.join_tables_and_modify_response(appointments)
     (
       appointments
