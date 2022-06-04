2f3dee1e678ed4624c25f7b8c76acdcfc7912160
nadavschwartz <nadavschwartz58@gmail.com>
2022-06-04 08:54:33 -0700

    update patient's model to add has_one :doctor per requirements instruction

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,6 +3,7 @@
 # For this test app, you can assume a patient has only one doctor
 class Patient < ApplicationRecord
   validates :name, uniqueness: true
+  has_one :doctor
   belongs_to :doctor
   has_many :appointments
 end
