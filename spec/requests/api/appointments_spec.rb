7d3e735968cf8e88b5a083024a32a16aa73a6bfd
nadavschwartz <nadavschwartz58@gmail.com>
2022-06-04 21:20:09 -0700

    initialized and implemented test cases for /api/appointments routes

---
+++ spec/requests/api/appointments_spec.rb
@@ -0,0 +1,135 @@
+require 'rails_helper'
+
+RSpec.describe 'Api/Appointments', type: :request do
+  it 'returns json object in a specific structure' do
+    FactoryBot.create(:appointment)
+    get api_appointments_path
+    expect(response.body).to match(
+      [{
+        id: Integer,
+        patient: {
+          name: String
+
+        },
+        doctor: {
+          name: String,
+          id: Integer
+        },
+        created_at: String,
+        start_time: String,
+        duration_in_minutes: Integer
+      }].to_json
+    )
+  end
+
+  it 'returns error messages status code for invalid past param' do
+    ['/api/appointments?past=bar', '/api/appointments?past=2', '/api/appointments?past='].each do |path|
+      get path
+      expect(response).to have_http_status(:bad_request)
+
+      expect(JSON.parse(response.body)['error']).to eq('past must be 0 or 1')
+    end
+  end
+
+  it 'returns error messages and status code for invalid page and length param' do
+    possible_errors = ['page and length must be integers bigger than 0', 'page and length must be present together']
+
+    ['/api/appointments?page=bar', '/api/appointments?page=2'].each do |path|
+      get path
+      expect(response).to have_http_status(:bad_request)
+
+      # except response.body to equal one of array possible_errors
+
+      expect(JSON.parse(response.body)['error']).to eq(possible_errors[1])
+    end
+
+    ['/api/appointments?page=1&length=bar', '/api/appointments?page=invalid&length=2', '/api/appointments?page=&length='].each do |path|
+      get path
+      expect(response).to have_http_status(:bad_request)
+
+      expect(JSON.parse(response.body)['error']).to eq(possible_errors[0])
+    end
+  end
+
+  it 'returns appointments in the future only for past=0 param' do
+    # create 2 doctors, each doctor has 2 patients, each patient has 4 appointments, two appointment is in the past and two in the future
+    2.times do
+      doctor = FactoryBot.create(:doctor)
+      2.times do
+        patient = doctor.patients.create(name: Faker::Name.unique.name)
+        2.times do
+          patient.appointments.create(patient: patient, doctor: doctor, duration_in_minutes: 50, start_time: Faker::Time.between(from: 2.days.from_now, to: 5.days.from_now))
+        end
+        2.times do
+          patient.appointments.create(patient: patient, doctor: doctor, duration_in_minutes: 50, start_time: Faker::Time.between(from: 5.days.ago, to: 2.day.ago))
+        end
+      end
+    end
+
+    get '/api/appointments?past=0'
+    expect(response).to have_http_status(:ok)
+
+    # sort appointments by start_time and check if first appointment is in the past
+    expect(JSON.parse(response.body)[0]['start_time'].to_date.future?).to eq(true)
+  end
+
+  it 'returns appointments in the past only for past=1 param' do
+    2.times do
+      doctor = FactoryBot.create(:doctor)
+      2.times do
+        patient = doctor.patients.create(name: Faker::Name.unique.name)
+        2.times do
+          patient.appointments.create(patient: patient, doctor: doctor, duration_in_minutes: 50, start_time: Faker::Time.between(from: 2.days.from_now, to: 5.days.from_now))
+        end
+        2.times do
+          patient.appointments.create(patient: patient, doctor: doctor, duration_in_minutes: 50, start_time: Faker::Time.between(from: 5.days.ago, to: 2.day.ago))
+        end
+      end
+    end
+
+    get '/api/appointments?past=1'
+    expect(response).to have_http_status(:ok)
+    # sort appointments by start_time and check if first appointment is in the past
+    expect(JSON.parse(response.body)[0]['start_time'].to_date.past?).to eq(true)
+  end
+
+  it 'returns 5 appointments from first page' do
+    2.times do
+      doctor = FactoryBot.create(:doctor)
+      2.times do
+        patient = doctor.patients.create(name: Faker::Name.unique.name)
+        2.times do
+          patient.appointments.create(patient: patient, doctor: doctor, duration_in_minutes: 50, start_time: Faker::Time.between(from: 2.days.from_now, to: 5.days.from_now))
+        end
+        2.times do
+          patient.appointments.create(patient: patient, doctor: doctor, duration_in_minutes: 50, start_time: Faker::Time.between(from: 5.days.ago, to: 2.day.ago))
+        end
+      end
+    end
+
+    get '/api/appointments?page=1&length=5'
+    expect(response).to have_http_status(:ok)
+    expect(JSON.parse(response.body).length).to eq(5)
+    expect(JSON.parse(response.body)[0]['id']).to eq(Appointment.first.id)
+  end
+
+  it 'returns 1 appointment from fourth page' do
+    2.times do
+      doctor = FactoryBot.create(:doctor)
+      2.times do
+        patient = doctor.patients.create(name: Faker::Name.unique.name)
+        2.times do
+          patient.appointments.create(patient: patient, doctor: doctor, duration_in_minutes: 50, start_time: Faker::Time.between(from: 2.days.from_now, to: 5.days.from_now))
+        end
+        2.times do
+          patient.appointments.create(patient: patient, doctor: doctor, duration_in_minutes: 50, start_time: Faker::Time.between(from: 5.days.ago, to: 2.day.ago))
+        end
+      end
+    end
+
+    get '/api/appointments?page=4&length=1'
+    expect(response).to have_http_status(:ok)
+    expect(JSON.parse(response.body).length).to eq(1)
+    expect(JSON.parse(response.body)[0]['id']).to eq(Appointment.fourth.id)
+  end
+end
