cc1e8dcb45a3102911df748f13f348122e46842a
nadavschwartz <nadavschwartz58@gmail.com>
2022-06-05 09:43:55 -0700

    removed hard coded seeding in doctors api test

--- spec/requests/api/doctors_spec.rb
+++ spec/requests/api/doctors_spec.rb
@@ -1,19 +1,6 @@
 RSpec.describe 'Api/Doctors', type: :request do
   it 'returns all doctors without appointments' do
     2.times do
-      doctor = FactoryBot.create(:doctor)
-      2.times do
-        patient = doctor.patients.create(name: Faker::Name.unique.name)
-        2.times do
-          patient.appointments.create(patient: patient, doctor: doctor, duration_in_minutes: 50, start_time: Faker::Time.between(from: 2.days.from_now, to: 5.days.from_now))
-        end
-        2.times do
-          patient.appointments.create(patient: patient, doctor: doctor, duration_in_minutes: 50, start_time: Faker::Time.between(from: 5.days.ago, to: 2.day.ago))
-        end
-      end
-    end
-
-    2.times do
       FactoryBot.create(:doctor)
     end
 
